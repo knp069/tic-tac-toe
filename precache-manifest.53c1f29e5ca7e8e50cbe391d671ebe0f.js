09061f36aa7a65fa098b3593811dadd97cfd5028
nishant <nishant.9c9@gmail.com>
2020-07-27 07:51:22 +0530

    Updates

---
+++ precache-manifest.53c1f29e5ca7e8e50cbe391d671ebe0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33356501c4bec77a241637fa3e84f09",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1035f7a36293709b9a74",
+    "url": "/tic-tac-toe/static/css/main.50023a25.chunk.css"
+  },
+  {
+    "revision": "d778af30de6e8db8420d",
+    "url": "/tic-tac-toe/static/js/2.a27fc20b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a27fc20b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1035f7a36293709b9a74",
+    "url": "/tic-tac-toe/static/js/main.4cfabfc9.chunk.js"
+  },
+  {
+    "revision": "fa4bbfb5b6a3f4445c7d",
+    "url": "/tic-tac-toe/static/js/runtime-main.912a2126.js"
+  }
+]);
